7e9065c0210620988307ba92932fd2b7adbd1ae3
Muhammad Asaad <meshmuhammadasaad@gmail.com>
2025-12-10 21:59:26 +0200

    add Identity Tables and using swaggerUI in our project

---
+++ SurveyBasket.Api/Presistence/EntitiesConfigurations/AppUserConfiguration.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SurveyBasket.Api.Presistence.EntitiesConfigurations;
+
+public class AppUserConfiguration : IEntityTypeConfiguration<AppUser>
+{
+    public void Configure(EntityTypeBuilder<AppUser> builder)
+    {
+        builder.Property(u => u.FirstName).HasMaxLength(100);
+        builder.Property(u => u.LastName).HasMaxLength(100);
+    }
+}
