1507577b0bec7e03781a68abfd01c05bbfc777e2
Muhammad Asaad <muhammad.as3d@gmail.com>
2026-02-08 22:24:43 +0200

    Add AuditableEntity to tracher tables

--- SurveyBasket.Api/Presistence/ApplicationDbContext.cs
+++ SurveyBasket.Api/Presistence/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace SurveyBasket.Api.Presistence;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IHttpContextAccessor httpContextAccessor)
     : IdentityDbContext<AppUser>(options)
 {
+    private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor; 
+
     public DbSet<Poll> Polls { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
@@ -13,5 +16,25 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
         base.OnModelCreating(builder);
     }
 
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var currentUserId = _httpContextAccessor.HttpContext!.User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+
+        var entries = ChangeTracker.Entries<AuditableEntity>();
+
+        foreach (var entity in entries)
+        {
+            if (entity.State == EntityState.Added)  
+                entity.Property(c => c.CreatedById).CurrentValue = currentUserId;
+
+            else if (entity.State == EntityState.Modified)
+            {
+                entity.Property(c => c.UpdatedById).CurrentValue = currentUserId;
+                entity.Property(c => c.UpdatedOn).CurrentValue = DateTime.UtcNow;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
 }
 
