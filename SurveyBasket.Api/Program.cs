b431e190c4af0eb9b5483acb406cf5c87c1a7172
Muhammad Asaad <muhammad.as3d@gmail.com>
2026-02-09 12:28:12 +0200

    Add CORS Policy for access origins

--- SurveyBasket.Api/Program.cs
+++ SurveyBasket.Api/Program.cs
@@ -6,7 +6,6 @@ builder.Services.AddDependencies(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline. 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -14,6 +13,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
