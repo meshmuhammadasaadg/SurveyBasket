b8db9bc37087a5c08b336e41a5c3c021d09549de
Muhammad Asaad <muhammad.as3d@gmail.com>
2026-03-13 04:13:31 +0200

    Add Results Services and endpoins

---
+++ SurveyBasket.Api/Contracts/Results/PollVotesResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace SurveyBasket.Api.Contracts.Results;
+
+public record PollVotesResponse(
+    string Title,
+    IEnumerable<VoteResponse> Votes
+);
+
+public record VoteResponse(
+    string VoterName,
+    DateTime VoteDate,
+    IEnumerable<QuestionAnswerResponse> SelectedAnswers
+);
+
+public record QuestionAnswerResponse(
+    string Question,
+    string Answers
+);
---
+++ SurveyBasket.Api/Contracts/Results/VotesPerDayResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace SurveyBasket.Api.Contracts.Results;
+
+public record VotesPerDayResponse(
+    DateOnly Date,
+    int NumberOfVotes
+);
+
