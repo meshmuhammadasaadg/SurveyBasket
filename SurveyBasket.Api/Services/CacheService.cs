abf97674cd12cb405ad5108eeb1ec5d91ea4e755
Muhammad Asaad <muhammad.as3d@gmail.com>
2026-03-22 01:55:36 +0200

    Add Distributed Cache in QuestionServices

---
+++ SurveyBasket.Api/Services/CacheService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace SurveyBasket.Api.Services;
+
+public class CacheService(IDistributedCache distributedCache) : ICacheService
+{
+    private readonly IDistributedCache _distributedCache = distributedCache;
+
+    public async Task<T?> GetAsync<T>(string key, CancellationToken cancellationToken = default) where T : class
+    {
+        var cacheValue = await _distributedCache.GetStringAsync(key, cancellationToken);
+
+        return cacheValue is null
+            ? null
+            : JsonSerializer.Deserialize<T>(cacheValue);
+    }
+
+    public async Task SetAsync<T>(string key, T value, CancellationToken cancellationToken = default) where T : class
+    {
+        await _distributedCache.SetStringAsync(key, JsonSerializer.Serialize(value), cancellationToken);
+    }
+
+    public async Task RemoveAsync(string key, CancellationToken cancellationToken = default)
+    {
+        await _distributedCache.RemoveAsync(key, cancellationToken);
+    }
+}
